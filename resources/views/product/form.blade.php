a11b805925ff01e72e43387dee9eb5d8b01112c5
Tihomir <raylight75@gmail.com>
2016-01-25 17:54:21 +0200

    crud

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -1,3 +1,8 @@
+<?php //$test = implode(",", $checkbox->size->lists("size_id")->all()); ?>
+<?php echo '<pre>',print_r($checkbox['size']),'</pre>'; ?>
+<?php echo '<pre>',print_r($sizes),'</pre>'; ?>
+<?php echo '<pre>',print_r($list),'</pre>'; ?>
+
 <div class="form-group">
     {!! Form::label('Slug', 'Slug:') !!}
     {!! Form::text('slug',null,['class'=>'form-control']) !!}
@@ -18,12 +23,13 @@
     {!! Form::label('Brand', 'Brand:') !!}
     {!! Form::select('brand_id', $brands, null, ['id' => 'brand_id','class'=>'form-control'])!!}
 </div>
-{{--<div class="form-group">
-    @foreach ($sizes as $s)
-    {!! Form::label($s->size,$s->size_id) !!}
-    {!! Form::checkbox( 'size[]',$s->size_id, null,['id' => $s->size_id,'class' => 'md-check'])!!}
+<div class="form-group">
+    <?php //echo '<pre>',print_r($sizes),'</pre>'; ?>
+    @foreach ($checkbox['size'] as $s)
+    {!! Form::label($s['size_id'],$s['size_id']) !!}
+    {!! Form::checkbox( 'size[]',$s['size_id'], in_array( 2, $checkbox['size']),['id' => $s['size_id'],'class' => 'md-check'])!!}
     @endforeach
-</div>--}}
+</div>
 <div class="form-group">
     {!! Form::label('Category', 'Category:') !!}
     {!! Form::text('cat_id',null,['class'=>'form-control']) !!}

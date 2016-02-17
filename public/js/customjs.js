146dbe2e79f37963fe131de8ce8937e670a0404d
raylight75@gmail.com <raylight75@gmail.com>
2016-02-17 21:10:53 +0200

    Blade Template

--- public/js/customjs.js
+++ public/js/customjs.js
@@ -1,18 +1,35 @@
-$(document).ready(function(){
-	$("li.pagination a, li.prev a, li.next a, li.active a ").click(function(e) {
-		 e.preventDefault();
-    $.ajax({
-      type: "GET",
-      url: $(this).attr('href'),
-      dataType: 'html',
-      success: function(html){
-        $("#container").html(html);
-        $("#blog").html(html);
-      }
-    });               
-    return false;
-  });            
+// Ajax pagination
+$(window).on('hashchange', function() {
+  if (window.location.hash) {
+    var page = window.location.hash.replace('#', '');
+    if (page == Number.NaN || page <= 0) {
+      return false;
+    } else {
+      getProducts(page);
+    }
+  }
 });
+$(function() {
+  $('#ajaxproducts').on('click', '.pagination a', function (e) {
+    var url = $(this).attr('href');
+    var hashes = url.split("?")[1];
+    var page = $(this).attr('href').split('page=')[1];
+    getProducts(hashes.split('page').reverse()[1],page);
+    e.preventDefault();
+  });
+});
+function getProducts(data,page) {
+  $.ajax({
+    url : '?' + data + 'page='+ page,
+    type: "GET",
+    dataType: 'json',
+  }).done(function (data) {
+    $('#ajaxproducts').html(data);
+  }).fail(function () {
+    alert('Posts could not be loaded.');
+  });
+}
+// End Ajax pagination
 $(function() {
 	$('#categories input:checkbox').on('click', function() {
 		if ($(this).is(":checked"))

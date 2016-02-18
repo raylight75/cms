0cff0511efb82ad856decd46180713073e879c27
Tihomir <raylight75@gmail.com>
2016-02-18 10:26:32 +0200

    crud

--- resources/views/frontend/product_container.blade.php
+++ resources/views/frontend/product_container.blade.php
@@ -8,8 +8,8 @@
                 <div class="tovar_img">
                     <div class="tovar_img_wrapper">
                         <img class="img" src="{{ url('images/products') }}/{{$row->a_img}}" alt=""/>
-                        <img class="img_h fancybox fancybox.ajax" href="{{ url('frame') }}/{{$row->product_id}}">
-                        <img class="img_h" src="{{ url('images/products') }}/{{$row->b_img}}" alt=""/>
+                        <img class="img_h fancybox fancybox.ajax" href="{{ url('frame') }}/{{$row->product_id}}"
+                             src="{{ url('images/products') }}/{{$row->b_img}}" alt=""/>
                     </div>
                     <div class="tovar_item_btns">
                         <a class="open-project tovar_view"

ee3fbe4521ed2e7609b9f90a1f194683f841b802
Tihomir <raylight75@gmail.com>
2016-02-25 16:17:04 +0200

    crud

--- public/js/productCtrl.js
+++ public/js/productCtrl.js
@@ -1,10 +1,15 @@
 var myApp = angular.module('myApp', ['angular-toArrayFilter']);
-    myApp.controller('productCtrl', function ($scope, $http) {
-        $http.get('api').success(function(data) {
-            console.log(data);
-                $scope.items = data;
+myApp.controller('productCtrl', function ($scope, $http) {
+        $scope.$watch('products', function () {
+            fetch();
+        });
+        function fetch() {
+            $http.get("api").then(function (response) {
+                console.log(response);
+                $scope.items = response.data.data;
+                $scope.orderProp = 'product_id';
+                $scope.latest = 6;
             });
-        $scope.orderProp = 'product_id';
-        $scope.latest = 6;
+        }
     }
 );

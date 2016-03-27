d6b7d349f37763706ed4bf4f5a533ffcf5285cac
raylight75@gmail.com <raylight75@gmail.com>
2016-03-27 19:55:32 +0300

    Admin backend

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,51 +1,64 @@
 @extends('layout/template')
 @section('content')
+    <style type="text/css">
+        ul li {
+            display: inline-block;
+            float: left;
+        }
+    </style>
     <h1>Book Show</h1>
     <form class="form-horizontal">
-        <div class="form-group">
+        <ul>
             <label for="image" class="col-sm-2 control-label">Front Image</label>
-            <div class="col-sm-10">
+            <li>
                 <img src="{{asset('images/products/'.$product->a_img)}}" height="120" width="90" class="img-rounded">
-            </div>
-        </div>
-        <div class="form-group">
+            </li>
+        </ul>
+        <ul>
             <label for="image" class="col-sm-2 control-label">Side Image</label>
-            <div class="col-sm-10">
+            <li>
                 <img src="{{asset('images/products/'.$product->b_img)}}" height="120" width="90" class="img-rounded">
-            </div>
-        </div>
-        <div class="form-group">
+            </li>
+        </ul>
+        <ul>
             <label for="image" class="col-sm-2 control-label">Left Image</label>
-            <div class="col-sm-10">
+            <li>
                 <img src="{{asset('images/products/'.$product->c_img)}}" height="120" width="90" class="img-rounded">
-            </div>
+            </li>
+        </ul>
+        <div class="form-group">
         </div>
         <div class="form-group">
             <label for="isbn" class="col-sm-2 control-label">ID</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="isbn" placeholder={{$product->product_id}} readonly>
             </div>
         </div>
         <div class="form-group">
             <label for="title" class="col-sm-2 control-label">Slug</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="title" placeholder={{$product->slug}} readonly>
             </div>
         </div>
         <div class="form-group">
             <label for="author" class="col-sm-2 control-label">Name</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="author" placeholder={{$product->name}} readonly>
             </div>
         </div>
         <div class="form-group">
             <label for="publisher" class="col-sm-2 control-label">Brand</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="publisher" placeholder={{$product->brands->brand}} readonly>
             </div>
         </div>
         <div class="form-group">
             <label for="publisher" class="col-sm-2 control-label">Size</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="publisher"
                        placeholder={{implode(",", $product->size->lists("size")->all())}} readonly>
@@ -53,18 +66,21 @@
         </div>
         <div class="form-group">
             <label for="publisher" class="col-sm-2 control-label">Category</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="publisher" placeholder={{$product->cat_id}} readonly>
             </div>
         </div>
         <div class="form-group">
             <label for="publisher" class="col-sm-2 control-label">Quantity</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="publisher" placeholder={{$product->quantity}} readonly>
             </div>
         </div>
         <div class="form-group">
             <label for="publisher" class="col-sm-2 control-label">Price</label>
+
             <div class="col-sm-10">
                 <input type="text" class="form-control" id="publisher" placeholder={{$product->price}} readonly>
             </div>

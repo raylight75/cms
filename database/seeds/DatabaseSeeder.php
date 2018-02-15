432c94fe30d2fbf3208dcd2c77caaf4cb35229b4
raylight75 <raylight75@gmail.com>
2018-02-15 14:16:48 +0200

    Add factory to fill database with random data for test.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,8 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
+     * Uncomment factory lines if you want to fill database with random data.
+     *
      * Run the database seeds.
      *
      * @return void
@@ -14,13 +16,15 @@ class DatabaseSeeder extends Seeder
         $this->call(BrandsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(ColorsTableSeeder::class);
-        $this->call(ColorProductTableSeeder::class);
+        //factory(App\Models\Colors_products::class, 600)->create();$this->call(ColorProductTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
         $this->call(CurrenciesTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
         $this->call(OrderStatusTableSeeder::class);
         $this->call(PaymentsTableSeeder::class);
+        //factory(App\Models\Product::class, 600)->create();
         $this->call(ProductsTableSeeder::class);
+        //factory(App\Models\Products_sizes::class, 600)->create();
         $this->call(ProductSizeTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
         $this->call(ShippingsTableSeeder::class);
